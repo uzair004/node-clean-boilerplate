5b33aceb7e46fd1c73b72df4d72e4724553fe300
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-02-04 17:13:31 +0500

    Create index.js

---
+++ src/domain/entities/user/index.js
@@ -0,0 +1,7 @@
+// user entities related factories will be imported & initialized here
+// e.g makeBuildUser factory should we provided neccessary dependencies & exported
+
+// const {makeBuildUser} = require('/user.js')
+// const makeUser = makeBuildUser({makeTs, makeId})
+
+// module.exports = {makeUser}
