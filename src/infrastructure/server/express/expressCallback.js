99486b4a73522ffebc328d45c1f3755eb6b0427d
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-02-06 02:14:59 +0500

    Create expressCallback to structurize http req, response

---
+++ src/infrastructure/server/express/expressCallback.js
@@ -0,0 +1,42 @@
+const makeCallback = (controller) => (req, res) => {
+  const httpRequest = {
+    body: req.body,
+    query: req.query,
+    params: req.params,
+    ip: req.ip,
+    method: req.method,
+    path: req.path,
+    user: req.user,
+    logger: req.logger,
+    source: {
+      ip: req.ip,
+      browser: req.get('User-Agent'),
+    },
+    headers: {
+      'Content-Type': req.get('Content-Type'),
+      Referer: req.get('referer'),
+      'User-Agent': req.get('User-Agent'),
+    },
+  };
+
+  controller(httpRequest)
+    .then((httpResponse) => {
+      if (httpResponse.headers) {
+        res.set(httpResponse.headers);
+      }
+      res.set('Content-Type', 'application/json');
+      res.type('json');
+      res.status(httpResponse.statusCode).send(httpResponse.body);
+    })
+    .catch((e) => {
+      res.status(400).send({
+        success: false,
+        code: 400,
+        error: {
+          description: e.message,
+        },
+      });
+    });
+};
+
+module.exports = { makeCallback };
