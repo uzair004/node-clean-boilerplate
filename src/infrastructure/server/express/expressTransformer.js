a1bdd438b138f8241b2d2a34a14eef01f35d810a
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-02-11 00:42:40 +0500

    use async await

--- src/infrastructure/server/express/expressTransformer.js
+++ src/infrastructure/server/express/expressTransformer.js
@@ -1,4 +1,4 @@
-const makeCallback = (controller) => (req, res) => {
+const makeCallback = (controller) => async (req, res) => {
   const httpRequest = {
     body: req.body,
     query: req.query,
@@ -19,24 +19,26 @@ const makeCallback = (controller) => (req, res) => {
     },
   };
 
-  controller(httpRequest)
-    .then((httpResponse) => {
-      if (httpResponse.headers) {
-        res.set(httpResponse.headers);
-      }
-      res.set('Content-Type', 'application/json');
-      res.type('json');
-      res.status(httpResponse.statusCode).send(httpResponse.body);
-    })
-    .catch((e) => {
-      res.status(400).send({
-        success: false,
-        code: 400,
-        error: {
-          description: e.message,
-        },
-      });
+  try {
+    const httpResponse = await controller(httpRequest);
+
+    if (httpResponse.headers) {
+      res.set(httpResponse.headers);
+    }
+
+    res.set('Content-Type', 'application/json');
+    res.type('json');
+    res.status(httpResponse.statusCode).send({
+      timestamp: new Date().valueOf(),
+      ...httpResponse.body,
     });
+  } catch (err) {
+    res.status(500).send({
+      timestamp: new Date().valueOf(),
+      success: false,
+      error: { code: 'INTERNAL_ERROR', description: err.message },
+    });
+  }
 };
 
 module.exports = makeCallback;
