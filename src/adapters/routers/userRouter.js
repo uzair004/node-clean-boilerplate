3ba580a8ea5a2136805094644c1aad051f3fcacb
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-02-11 23:09:58 +0500

    Update userRouter: pass signUpC

--- src/adapters/routers/userRouter.js
+++ src/adapters/routers/userRouter.js
@@ -1,27 +1,11 @@
+const { signUpC } = require('../controllers/');
+
 module.exports = ({ webFramework, transformer }) => {
   const router = webFramework.Router();
 
   // Define your user routes here
 
-  router.post('/signup', transformer(signUpController));
+  router.post('/signup', transformer(signUpC));
 
   return router;
 };
-
-async function signUpController() {
-  return {
-    statusCode: 200,
-    body: {
-      user: {
-        name: 'me',
-        email: 'me@mail.com',
-      },
-    },
-  };
-  //   return {
-  //     statusCode: 400,
-  //     body: {
-  //       error: { code: 'TESTING', message: 'Testing Error Response' },
-  //     },
-  //   };
-}
